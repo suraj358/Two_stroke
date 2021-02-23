e499a8104592c78f2e381575794b8e958e6d7669
Suraj <60735768+suraj358@users.noreply.github.com>
2021-02-23 12:54:21 +0530

    1.0

---
+++ sins/index.js
@@ -0,0 +1,23 @@
+
+
+function rotation()
+{
+    time=theta/(6*rpm);
+
+}
+
+var F=0;
+
+FP = 
+
+function varupdate()
+{
+    if(theta<=Math.PI)
+        {
+            F=FP-FI+W;
+        }
+}
+
+
+
+
